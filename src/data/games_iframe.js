961f7324192c2a6483ff13d20c9b3bea496c52f7
owaispkr <owaispkr@users.noreply.github.com>
2020-08-17 16:45:14 +1200

    game added

--- src/data/games_iframe.js
+++ src/data/games_iframe.js
@@ -18,13 +18,31 @@ const games_iframe = [
       "Little Big Runners is an endless runner game that has the ability to grow bigger or become smaller to find a way through the obstacles and escape the chasing monster.",
   },
   {
-    iframe:
-      "<iframe src='https://wanted5games.com/games/html5/dune-surfer-new-en-s-iga-cloud/index.html?pub=10' name='cloudgames-com' width='590' height='360' frameborder='0' scrolling='no'></iframe>",
-    name: "Dune Surfer",
     imgsrc:
-      "https://s3-eu-west-1.amazonaws.com/wanted5games-games-live/game-img/8590.png",
+      "https://s3-eu-west-1.amazonaws.com/wanted5games-games-live/game-img/8562.png",
+    name: "Jungle Bricks",
+    iframe:
+      "<iframe src='https://wanted5games.com/games/html5/jungle-bricks-new-en-s-iga-cloud/index.html?pub=10' name='cloudgames-com' width='500' height='695' frameborder='0' scrolling='no'></iframe>",
     description:
-      "It’s a ball.. it’s a stickman… it’s a dune surfer! Go from ball, to stickman, to ball, to stickman as you draw, surf and slide your way out of the dunes, dune surfer stickman dude!",
+      "The Lihzahrd Brick is the building block of Jungle Temples. Lihzahrd Brick can be made into furniture at a Lihzahrd Furnace",
+  },
+  {
+    imgsrc:
+      "https://s3-eu-west-1.amazonaws.com/wanted5games-games-live/game-img/8584.png",
+    name: "Jumpdown",
+    iframe:
+      "<iframe src='https://wanted5games.com/games/html5/jump-down-new-en-s-iga-cloud/index.html?pub=10' name='cloudgames-com' width='500' height='727' frameborder='0' scrolling='no'></iframe>",
+    description:
+      "One-tap easy-to-learn controls, rich visual effects and addictive gameplay mechanics. Carefully maneuver through tiles, or meet your doom!",
+  },
+  {
+    imgsrc:
+      "https://s3-eu-west-1.amazonaws.com/wanted5games-games-live/game-img/8550.png",
+    name: "Darts Pro",
+    iframe:
+      "<iframe src='https://wanted5games.com/games/html5/darts-pro-new-en-s-iga-cloud/index.html?pub=10' name='cloudgames-com' width='670' height='600' frameborder='0' scrolling='no'></iframe>",
+    description:
+      "One-tap easy-to-learn controls, rich visual effects and addictive gameplay mechanics. Carefully maneuver through tiles, or meet your doom!",
   },
 ];
 
