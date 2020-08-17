0e4f16177d835d92e37058977a2d7f74bbe9a626
owaispkr <owaispkr@users.noreply.github.com>
2020-08-17 16:17:06 +1200

    Updates

---
+++ precache-manifest.6e87a9d69da4a090dee4a4263ddc5391.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5e2e49ef5b8b0a9d37e20c5d8e3a178",
+    "url": "/kidszone/index.html"
+  },
+  {
+    "revision": "e483993d386ab5497760",
+    "url": "/kidszone/static/js/2.c368f0f3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/kidszone/static/js/2.c368f0f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e28c0bec630b8eab1401",
+    "url": "/kidszone/static/js/main.5cdfb061.chunk.js"
+  },
+  {
+    "revision": "9cf3a7ff011a44df23dd",
+    "url": "/kidszone/static/js/runtime-main.7d49f120.js"
+  },
+  {
+    "revision": "1b91038dc1e4119f154d9c8a242bafda",
+    "url": "/kidszone/static/media/kidZone_logo.1b91038d.png"
+  }
+]);
